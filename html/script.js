d5307d3e923fe2d2aaa33d2d2496a158e8177aa0
Roan Vickerman <29734170+14ROVI@users.noreply.github.com>
2022-02-21 02:26:09 +0000

    removed unesecary commands

--- html/script.js
+++ html/script.js
@@ -45,23 +45,20 @@ async function shortenurl() {
 }
 
 
-// Make the DIV element draggable:
 dragElement(document.getElementById("draggable-window"));
 
 function dragElement(elmnt) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
     if (document.getElementById(elmnt.id + "-header")) {
-        // if present, the header is where you move the DIV from:
         document.getElementById(elmnt.id + "-header").onmousedown = dragMouseDown;
     } else {
-        // otherwise, move the DIV from anywhere inside the DIV:
         elmnt.onmousedown = dragMouseDown;
     }
 
     function dragMouseDown(e) {
         e = e || window.event;
         e.preventDefault();
-        // get the mouse cursor position at startup:
+
         var mx = e.clientX;
         var my = e.clientY;
         var rect = elmnt.getBoundingClientRect();
@@ -69,7 +66,6 @@ function dragElement(elmnt) {
         offsety = my - rect.y
 
         document.onmouseup = closeDragElement;
-        // call a function whenever the cursor moves:
         document.onmousemove = elementDrag;
     }
 
@@ -96,7 +92,6 @@ function dragElement(elmnt) {
     }
 
     function closeDragElement() {
-        // stop moving when mouse button is released:
         document.onmouseup = null;
         document.onmousemove = null;
     }
